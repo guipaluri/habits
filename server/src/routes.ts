74f12a3812d23e80e9f9b40548f0d0c4faac1b67
guipaluri <guilhermepaluri@hotmail.com>
2023-02-02 21:02:42 -0300

    Upgraded backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -30,4 +30,44 @@ export async function appRoutes(app: FastifyInstance) {
             }
         })
     })
+
+    app.get('/day', async (req) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
+
+        const { date } = getDayParams.parse(req.query)
+
+        const parsedDate = dayjs(date).startOf('day')
+        const weekDay = parsedDate.get('day')
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date,
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay,
+                    }
+                }
+            }
+        })
+
+        const day = await prisma.day.findUnique({
+            where: {
+                date: parsedDate.toDate(),
+            },
+            include: {
+                dayHabits: true
+            }
+        })
+
+        const completedHabits = day?.dayHabits.map(dayHabit => dayHabit.habit_id)
+
+        return {
+            possibleHabits,
+            completedHabits
+        }
+    })
 }
---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,11 @@
+export function SummaryTable() {
+    return (
+        <div className="w-full flex">
+            <div className="grid grid-rows-7 grid-flow-row gap-3">
+                <div className="text-zinc-400 text-xl h-10 w-10 flex items-center justify-center">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
